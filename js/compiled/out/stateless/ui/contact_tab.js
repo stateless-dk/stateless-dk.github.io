4f6f4ad1dca123166459081ba43f13265fb73dda
Jonas Green <jonasgreen12345@gmail.com>
2017-10-09 20:41:54 +0200

    remove out files

--- js/compiled/out/stateless/ui/contact_tab.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.9.946 {}
-goog.provide('stateless.ui.contact_tab');
-goog.require('cljs.core');
-stateless.ui.contact_tab.render = (function stateless$ui$contact_tab$render(){
-return null;
-});
-
-//# sourceMappingURL=contact_tab.js.map?rel=1507562209092
